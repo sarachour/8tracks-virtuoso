eedda4ea8400e83208217d8376c92eb22dd90be5
sachour <sachour@mit.edu>
2014-05-14 17:43:56 -0400

    added record icons

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,29 +1,44 @@
 function Playlist(){
-	this.songs = [];
 	this.spotify = new Spotify();
+	this.save = function(){
+		localStorage.setItem("playlist_cache", JSON.stringify(this.songs));
+	}
+	this.load = function(){
+		if(localStorage.hasOwnProperty("playlist_cache")){
+			this.songs = JSON.parse(localStorage.getItem("playlist_cache"));
+			console.log(this.songs);
+			//this.songs = {};
+		}
+		else{
+			this.songs = {};
+		}
+	}
 	this.add = function(artist, name){
-		songdata = {};
-		songdata.artist = artist;
-		songdata.name = name;
 		plist = this;
+		if(!plist.songs.hasOwnProperty(artist)){
+			plist.songs[artist] = {};
+		}
+		if(!plist.songs[artist].hasOwnProperty(name)){
+			plist.songs[artist][name] = {};
+		}
 		this.spotify.search(artist, name, function(data){
-			console.log(data);
 			if(data.tracks.length > 0){
-				track = data.tracks[0];
-				songdata.spotify = {};
-				songdata.spotify.track_id = track.href;
-				console.log(songdata);
-				plist.songs.push(songdata);
+				plist.songs[artist][name].spotify = {track_id: data.tracks[0].href};
+				plist.save();
 			}
 		})
-		
+				
 
 	}
 	this.getSpotify = function(){
 		playlist = "";
-		this.songs.forEach(function(song){
-			playlist += song.spotify.track_id + "\n";
-		})
+		for(var artist in this.songs){
+			for(var track in this.songs[artist]){
+				if(this.songs[artist][track].hasOwnProperty("spotify")){
+					playlist += this.songs[artist][track].spotify.track_id+"\n";
+				}
+			}
+		}
 		return playlist;
 	}
 	this.getItunes = function(){
@@ -34,12 +49,14 @@ function Playlist(){
 	}
 	this.getPlain = function(){
 		playlist = "";
-		this.songs.forEach(function(song){
-			console.log(song);
-			playlist += song.artist + "\t"+song.name + "\n";
-		});
+		for(var artist in this.songs){
+			for(var track in this.songs[artist]){
+					playlist += artist + "\t"+track + "\n";
+			}
+		}
 		return playlist;
 	}
+	this.load();
 
 
 }
