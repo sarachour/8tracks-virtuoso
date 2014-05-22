f2b7640dc6e3fe9d1012ccd96e2f5547d1bac15c
sachour <sachour@mit.edu>
2014-05-22 17:39:52 -0400

    layout

--- scripts/uiv2.js
+++ scripts/uiv2.js
@@ -1,33 +1,60 @@
-document.addEventListener('DOMContentLoaded', function() {
-    jQuery(function($) {
-        // Just call the inner layout once to initialize it. This
-        // must happen before the outer layout is initialized. It
-        // will be automatically resized when the outer layout is
-        // resized.
-        $('#player-controls').layout();
-        $('#player-title').layout();
-        var outerContainer = $('#player');
 
-        function layout() {
-          outerContainer.layout({resize: false});
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+        if(request.action == "update"){
+            userInterface.updateView();
         }
-        layout();
+  }
+)
+ 
+function SetupLayout(){
+  $('#player-controls').layout();
+  $('#player-title').layout();
+  var outerContainer = $('#player').layout({resize: false});
 
 
-        $(window).resize(layout);
+}
+function SetupJQuery(){
+   //play bind
+   $("#player_play").click(function(){
+      if($("#player_play").hasClass("playing")){
+        $("#player_play").removeClass("playing");
+        chrome.extension.sendMessage({action: "pause"});
+        $("#player_play").attr("src", "images/play.png");
+      }
+      else{
+        $("#player_play").addClass("playing");
+        $("#player_play").attr("src", "images/pause.png");
+        chrome.extension.sendMessage({action: "resume"});
+      }
+    });
+   //like bind
+    $("#player_like_mix").click(function(){
+      if($("#player_like_mix").hasClass("like")){
+        chrome.extension.sendMessage({action: "unlike-mix"})
+      }
+      else{
+        chrome.extension.sendMessage({action: "like-mix"})
+      } 
+    })
+    //star bind
+    $("#player_star_track").click(function(){
+      if($("#player_star_track").hasClass("favorite")){
+        chrome.extension.sendMessage({action: "unfavorite-track"})
+      }
+      else{
+        chrome.extension.sendMessage({action: "favorite-track"})
+      } 
+    })
 
-        // This selector is changed to only select the west and east
-        // components of the outer layout.
-        $('.layout-outer > .east').resizable({
-          handles: 'w',
-          stop: layout
-        });
-
-        $('.layout-outer > .west').resizable({
-          handles: 'e',
-          stop: layout
-        });
+    $( "#player_prog" ).on('change', function() { 
+        pct = $("#player_prog").val()/100.0; 
+        chrome.extension.sendMessage({action: "set-time", percent:pct})
       });
+}
+document.addEventListener('DOMContentLoaded', function() {
+  SetupLayout();
+  SetupJQuery();  
 
 
 })
