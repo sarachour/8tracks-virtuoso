7ef9a90fc6a0dff8cf05fbc34bbe7c76b6f184a5
sachour <sachour@mit.edu>
2014-05-22 11:56:41 -0400

    Tet

---
+++ scripts/uiv2.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  jQuery(function($) {
+        // Just call the inner layout once to initialize it. This
+        // must happen before the outer layout is initialized. It
+        // will be automatically resized when the outer layout is
+        // resized.
+        $('.layout-inner').layout();
+
+        var outerContainer = $('.layout-outer');
+        
+        function layout() {
+          outerContainer.layout({resize: false});
+        }
+        layout();
+
+        $(window).resize(layout);
+
+        // This selector is changed to only select the west and east
+        // components of the outer layout.
+        $('.layout-outer > .east').resizable({
+          handles: 'w',
+          stop: layout
+        });
+
+        $('.layout-outer > .west').resizable({
+          handles: 'e',
+          stop: layout
+        });
+      });
+})
