3b76eff9e7c2e3384ab85dac233ab4a48cf97c0e
sachour <sachour@mit.edu>
2016-05-19 15:03:28 -0400

    F

--- src/globals.js
+++
@@ -1,8 +0,0 @@
-
-
-var GLOBALS = {
-   "8tracks-api-key":"5f1ed8e86c7f6792bdd581ce587e996ce8b00ae2",
-   "spotify-api-key":"9587d3dc3e1d40828a179b633008c7ab", //key
-   "spotify-api-secret": "b22c58fa694941cc844218c04e509980", //secret
-   "spotify-scope":"playlist-read-private playlist-modify-private"
-}
